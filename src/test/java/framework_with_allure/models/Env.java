7ece0acbf5eec71e3a155c1ef02201adf6ac5bac
Ramisa490 <152860747+Ramisa490@users.noreply.github.com>
2024-01-09 11:42:07 +0600

    -

---
+++ src/test/java/framework_with_allure/models/Env.java
@@ -0,0 +1,13 @@
+package framework_with_allure.models;
+
+import lombok.Getter;
+
+@Getter
+public class Env {
+    private String env;
+
+    public String getEnv()
+    {
+        return  env;
+    }
+}
